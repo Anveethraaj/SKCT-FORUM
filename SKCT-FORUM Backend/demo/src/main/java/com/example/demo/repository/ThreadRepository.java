50b5ffb6ddfd48b9be6cb421324e57633b3b23a6
Anveethraaj <119087790+Anveethraaj@users.noreply.github.com>
2024-12-12 23:19:44 +0530

    Add files via upload

---
+++ SKCT-FORUM Backend/demo/src/main/java/com/example/demo/repository/ThreadRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Thread;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface ThreadRepository extends JpaRepository<Thread, Long> {
+// }
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThreadRepository extends JpaRepository<Thread, Long> {
+}
