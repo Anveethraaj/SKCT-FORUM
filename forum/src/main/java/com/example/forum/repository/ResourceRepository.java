9e335aa845383129a23f6a88a344208c899a4a8b
Anveethraaj <119087790+Anveethraaj@users.noreply.github.com>
2024-12-12 23:02:43 +0530

    Delete forum directory

--- forum/src/main/java/com/example/forum/repository/ResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.forum.repository;
-
-import com.example.forum.model.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Resource, Integer> {
-}
--- forum/src/main/java/com/example/forum/repository/ThreadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.forum.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.forum.model.Thread;
-
-public interface ThreadRepository extends JpaRepository<Thread, Integer> {
-}
